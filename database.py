267c0d2378750444922737934d15c67067818f72
BrianSohn <64431566+BrianSohn@users.noreply.github.com>
2024-01-22 02:26:13 +0000

    added dynamic pages for each job

--- database.py
+++ database.py
@@ -15,3 +15,12 @@ def load_jobs_from_db():
     result = conn.execute(text("select * from jobs"))
     jobs = [row._asdict() for row in result.all()]
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn: 
+    result = conn.execute(text("select * from jobs where id = :val"), {"val": id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else: 
+      return rows[0]._asdict()
